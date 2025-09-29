e0dc5f441e11ebf95597aad682f84f314967867f
Roromo0306 <pownyrainbow03@gmail.com>
2025-09-29 12:38:12 +0200

    Mejora Mapa
    Añadido un panel que previamente te pregunta si deseas salir del restaurante

--- InfernoFeast/Assets/Scripts/ExitRestaurant.cs
+++ InfernoFeast/Assets/Scripts/ExitRestaurant.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Map : MonoBehaviour
+public class ExitRestaurant : MonoBehaviour
 {
     private GameObject mapPanel;
+    private GameObject exitPanel;
 
     // Start is called before the first frame update
     void Start()
     {
         mapPanel = GameObject.Find("Map");
         mapPanel.SetActive(false);
+
+        exitPanel = GameObject.Find("Exit Restaurant Panel");
+        exitPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,7 +27,18 @@ public class Map : MonoBehaviour
     {
         if(collider.tag == "Player")
         {
-            mapPanel.SetActive(true);
+            exitPanel.SetActive(true);
         }
+    }
+
+    public void Yes()
+    {
+        exitPanel.SetActive(false);
+        mapPanel.SetActive(true);
+    }
+
+    public void No()
+    {
+        exitPanel.SetActive(false);
     }
 }
