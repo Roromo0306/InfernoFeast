81620a14f19f82c31df91579379fe9c75372aea3
Roromo0306 <pownyrainbow03@gmail.com>
2025-09-29 11:30:13 +0200

    Player Controller Final
    Movimiento del player final, usando cinemachine

--- InfernoFeast/Assets/Scripts/Player/PlayerController.cs
+++ InfernoFeast/Assets/Scripts/Player/PlayerController.cs
@@ -1,28 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Atributos del Player")]
-    public float speed = 5f;
+    [Header("Movimiento del jugador")]
+    public float moveSpeed = 5f;
+    public Rigidbody rb;
 
-    private float MoveX, MoveZ;
-    private Rigidbody rb;
+    private float horizontalInput;
+    private float verticalInput;
 
-    // Start is called before the first frame update
+    [Header("Cámara")]
+    public Transform cameraTransform; // Cámara que seguirá al jugador
+    public Vector3 cameraOffset = new Vector3(0, 5, -7);
+    public float cameraFollowSpeed = 5f;
+    public bool lookAtPlayer = true;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        if (rb == null) rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        if (cameraTransform == null && Camera.main != null)
+        {
+            cameraTransform = Camera.main.transform;
+        }
+            
     }
 
-    // Update is called once per frame
     void Update()
     {
-        MoveX = Input.GetAxis("Vertical");
-        MoveZ = Input.GetAxis("Horizontal");
-
-        Vector3 movimiento = new Vector3(MoveX, 0, MoveZ).normalized;
-        rb.velocity = movimiento*speed;
+        // Leer inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
     }
+
+    void FixedUpdate()
+    {
+        // Movimiento en ejes globales
+        Vector3 moveDirection = new Vector3(verticalInput, 0f, horizontalInput).normalized;
+        rb.velocity = moveDirection * moveSpeed + new Vector3(0, rb.velocity.y, 0);
+
+        if (cameraTransform != null)
+        {
+            // Seguir al jugador con suavizado
+            Vector3 targetPosition = transform.position + cameraOffset;
+            cameraTransform.position = Vector3.Lerp(
+                cameraTransform.position,
+                targetPosition,
+                cameraFollowSpeed * Time.deltaTime
+            );
+
+            if (lookAtPlayer)
+                cameraTransform.LookAt(transform.position);
+        }
+    }
+
+    
 }
+
+
+    
