dda40c5b0786cf3c70593d9ed099d1009751306f
Alonso2880 <40578797+Alonso2880@users.noreply.github.com>
2025-10-20 20:34:31 +0200

    Mecanica de cortar
    Añadida la mecánica de cortar

--- InfernoFeast/Assets/Scripts/Restaurant/CutCounter.cs
+++ InfernoFeast/Assets/Scripts/Restaurant/CutCounter.cs
@@ -5,22 +5,57 @@ using UnityEngine.UI;
 
 public class CutCounter : MonoBehaviour
 {
-    [Header("Slider")]
-    public Slider progressBar; //Barra de progreso
-    public float progressIncrement = 0.1f; //Lo que aumenta por accion
-    public float maxProgress = 100f; //Progreso maximo
+    [Header("Slider")] 
+    public Slider slider; //Slider
+    public KeyCode primeraTecla = KeyCode.Mouse0; //Boton izquierdo del raton
+    public KeyCode segundaTecla = KeyCode.Mouse1; //Boton derecho del raton
 
-    public GameObject PadrePlayer;
+    private bool interaccionAcriva = false; //Bool para comprobar si la mecanica se ha activado
+    private bool IzquierdaClick = false; //Bool para comprobar si se ha dado click al boton izquierdo del raton
 
-    private float currentProgress = 0f;
-    private bool isInteracting = false;
-    private int lastMouseButton = -1;
-    private int Indice;
+    [Header("Padres")]
+    public GameObject PadrePlayer; //Padre del player
+    public GameObject PadreCortar; //Padre del counter
+
+    private int Indice; //Donde se guardara el indice de la lista para instanciar desde la otra lista
     private bool ObjetoEncontrado = false; //Con este bool detectare si se ha encontrado un nombre en el if
+    private GameObject HijoCortar; //Donde se guarda el objeto hijo al dejarlo para pasarlo como referencia a la funcion de terminar.
 
     [Header("Listas")]
-    public List<TipoIngrediente> cortados;
-    public List<TipoIngrediente> ingredientes;
+    public List<TipoIngrediente> cortados; //Lista de los objetos cortados
+    public List<TipoIngrediente> ingredientes; //Lista de los ingredientes
+
+    private void Awake()
+    {
+        slider.gameObject.SetActive(false); //Activo el slider
+        slider.maxValue = 8; //Establezco en 8 el valor maximo del slider
+    }
+    private void Update()
+    {
+        if (!interaccionAcriva) return; //Revisa si la esta funionando la mecanica de cortar y si no lo esta para y no continua
+
+        if (!IzquierdaClick) //Click izquierdo
+        {
+            if(Input.GetKeyDown(primeraTecla))
+            {
+                IzquierdaClick = true;
+                slider.value += 1;
+            }
+        }
+        else //Click derecho
+        {
+            if (Input.GetKeyDown(segundaTecla))
+            {
+                slider.value += 1;
+                IzquierdaClick = false;
+            }
+        }
+
+        if(slider.value >= slider.maxValue) //Pone fin a la iteracion cuando el valor del slider llega a 8
+        {
+            FinInteraccion(HijoCortar);
+        }
+    }
 
     public void cortar()
     {
@@ -37,6 +72,26 @@ public class CutCounter : MonoBehaviour
             }
         }
 
+        HijoCortar = Instantiate(HijoPadre, PadreCortar.transform.position, HijoPadre.transform.rotation, PadreCortar.transform); //Intancia el objeto en el counter
+        HijoCortar.name = HijoPadre.name; //Le da al objeto el nombre correcto
+        Destroy(HijoPadre); //Destruye el objeto
+
+        Empezar();
+
+    }
+
+    //Funcion para empezar la accion con el slider
+    private void Empezar()
+    {
+        interaccionAcriva = true;
+        IzquierdaClick = false;
+        slider.value = slider.minValue;
+        slider.gameObject.SetActive(true);
+    }
+
+    //Funcion para terminar la accion y que instancia el objeto cortado
+    private void FinInteraccion(GameObject HijoPadre)
+    {
         //Si el bool es true pasa lo siguiente
         if (ObjetoEncontrado)
         {
@@ -56,5 +111,8 @@ public class CutCounter : MonoBehaviour
             Destroy(HijoPadre); //Destruyo el objeto que llevaba el jugador
         }
 
+        interaccionAcriva = false;
+        slider.gameObject.SetActive(false);
+        HijoCortar = null;
     }
 }
