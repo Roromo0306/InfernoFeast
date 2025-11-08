1197fe1b0767c0cefd6e727d47870a252a9e36d9
Alonso2880 <40578797+Alonso2880@users.noreply.github.com>
2025-11-08 13:54:26 +0100

    Arreglos al código

--- InfernoFeast/Assets/Scripts/Restaurant/MixCounter.cs
+++ InfernoFeast/Assets/Scripts/Restaurant/MixCounter.cs
@@ -118,50 +118,8 @@ public class MixCounter : MonoBehaviour
             Indice = 0;
             ObjetoEncontrado = false;
         }
-        else
-        {
-            /*GameObject nuevoObjeto = Instantiate(HijoPadre, PadrePlayer.transform.position, HijoPadre.transform.rotation, PadrePlayer.transform); //Instancio el mismo objeto que llevaba el jugador
-            nuevoObjeto.name = HijoPadre.name; //Me aseguro que el nombre sea el correcto
-
-            Destroy(HijoPadre); //Destruyo el objeto que llevaba el jugador*/
-        }
 
         isInteracting = false;
         progressBar.gameObject.SetActive(false);
     }
-
-   /* public void Batir()
-    {
-        GameObject HijoPadre = PadrePlayer.transform.GetChild(0).gameObject; //Guardamos el gameobject que carga el player en un gameobject nuevo
-
-        //Con este for recorre la lista entera hasta que encuentra un objeto que se llama igual que el objeto que lleva el jugador. Al encontrar esto, activo el bool y guardo el indice
-        for (int i = 0; i < ingredientes.Count; i++)
-        {
-            if (ingredientes[i].name == HijoPadre.name)
-            {
-                Indice = i;
-                ObjetoEncontrado = true;
-                break;
-            }
-        }
-
-        //Si el bool es true pasa lo siguiente
-        if (ObjetoEncontrado)
-        {
-            Destroy(HijoPadre); //Destruyo el objeto que llevaba el jugador
-
-            GameObject nuevoObjeto = Instantiate(batidos[Indice].prefabIngrediente, PadrePlayer.transform.position, batidos[Indice].prefabIngrediente.transform.rotation, PadrePlayer.transform); //Instancio el objeto equivalente en la lista de batidos
-            nuevoObjeto.name = batidos[Indice].prefabIngrediente.name; //Me aseguro que el nombre del nuevo objeto instanciado sea el correcto
-
-            Indice = 0;
-            ObjetoEncontrado = false;
-        }
-        else
-        {
-            GameObject nuevoObjeto = Instantiate(HijoPadre, PadrePlayer.transform.position, HijoPadre.transform.rotation, PadrePlayer.transform); //Instancio el mismo objeto que llevaba el jugador
-            nuevoObjeto.name = HijoPadre.name; //Me aseguro que el nombre sea el correcto
-
-            Destroy(HijoPadre); //Destruyo el objeto que llevaba el jugador
-        }
-    }*/
 }
