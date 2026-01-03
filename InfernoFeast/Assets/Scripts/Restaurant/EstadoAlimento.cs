a4967f71c191e0d42f227776380ece11b97050b2
Alonso2880 <40578797+Alonso2880@users.noreply.github.com>
2026-01-03 20:37:47 +0100

    Nuevos arreglos
    -Se ha añadido un componente a los ingredientes y sus formas cocinadas para que se puedan identificar mediante un número. Esto permite que si tienes una carne frita no puedas freírla otra vez. Solo se ha implementado esto en la freidora, mañana se implementará en los demás

---
+++ InfernoFeast/Assets/Scripts/Restaurant/EstadoAlimento.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstadoAlimento : MonoBehaviour
+{
+    /*
+     Depende del valor de estado sigficara una cosa u otra:
+     0: Nada
+     1: Cortado
+     2: Frito
+     3: Hervido
+     4: Horneado
+     5: Batido
+     6: Quemado
+     7: Receta Lista
+     */
+    public int estado;
+}
