7b2978af56b41982cd214d180d786086170f3b7c
Roromo0306 <pownyrainbow03@gmail.com>
2026-01-12 13:50:30 +0100

    Añadido sonido de fondo Boss1

--- InfernoFeast/Assets/Scripts/Boss/Cinematic/VideoCinematic.cs
+++ InfernoFeast/Assets/Scripts/Boss/Cinematic/VideoCinematic.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
@@ -8,7 +8,9 @@ public class VideoCinematic : MonoBehaviour
     public VideoPlayer videoPlayer;
     public GameObject cinematica;
     public DialogueBoss dialogueBoss;
-    
+
+    public AudioSource musicAudioSource;   // AudioSource que reproducirá la música
+    public AudioClip backgroundMusic;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +21,26 @@ public class VideoCinematic : MonoBehaviour
     // Update is called once per frame
     void VideoEnd(VideoPlayer vp)
     {
+        // Ocultamos la cinemática
         cinematica.SetActive(false);
 
+        // Empezamos el diálogo
         List<string> dialogo = new List<string>()
         {
-            "Bienvenido seas Cedrik!",
-            "Espero que disfrutes la experiencia.",
-            "�Demuestra de lo que eres capaz!"
+        "Bienvenido seas Cedrik!",
+        "Espero que disfrutes la experiencia.",
+        "¡Demuestra de lo que eres capaz!"
         };
 
         dialogueBoss.StartDialog(dialogo);
-        
+
+        // 🔹 Reproducir música en loop
+        if (musicAudioSource != null && backgroundMusic != null)
+        {
+            musicAudioSource.clip = backgroundMusic;
+            musicAudioSource.loop = true;
+            musicAudioSource.Play();
+        }
+
     }
 }
