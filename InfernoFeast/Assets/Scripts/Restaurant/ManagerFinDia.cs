414f82b9a62dbd4e6b07e315790ded22042973bf
Alonso2880 <40578797+Alonso2880@users.noreply.github.com>
2026-03-03 11:30:08 +0100

    UI dinero y reputacion

--- InfernoFeast/Assets/Scripts/Restaurant/ManagerFinDia.cs
+++ InfernoFeast/Assets/Scripts/Restaurant/ManagerFinDia.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ManagerFinDia : MonoBehaviour
 {
@@ -10,6 +11,10 @@ public class ManagerFinDia : MonoBehaviour
     public int dinero;
     public int reputacion;
 
+    [Header("UI Stats")]
+    public TMP_Text din;
+    public TMP_Text reput;
+
     private void Awake()
     {
         if (Instance == null)
@@ -18,5 +23,11 @@ public class ManagerFinDia : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
         else Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        din.text = dinero.ToString("F0");
+        reput.text = reputacion.ToString("F0");
     }
 }
